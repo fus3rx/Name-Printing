445f673d19c3f91903cc772ea6b16b0a1cf3a244
fus3rx <imaginersystems>
2016-10-29 14:54:07 +0600

    name printing through console using method

---
+++ Name Printing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Name_Printing
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+            String firstName;
+            String secondName;
+
+            firstName= Console.ReadLine();
+            secondName = Console.ReadLine();
+            String myName= MyName(firstName,secondName);
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+
+        static String MyName(String fName, String sName)
+        {
+            return fName+" "+sName;
+        }
+    }
+}
